c1cb08d360db2e6c04804eaaa75d7d553633226c
GeriCookie <gergana.bailova@gmail.com>
2015-09-02 16:18:54 +0300

    Added change book status buttons and functionality

--- public/app/app.js
+++ public/app/app.js
@@ -1,39 +1,47 @@
 import data from 'app/data/data';
 import handlebars from 'bower_components/handlebars/handlebars';
 
-var sammyApp = Sammy('#content', function () {
+var sammyApp = Sammy('#content', function() {
 
-  this.get('#/', function () {
+  this.get('#/', function() {
     console.log('----HOME');
   });
 
-  this.get('#/books', function () {
+  this.get('#/books', function() {
     var params = {
       author: this.params.author,
       genre: this.params.genre
     };
 
     data.books.get(params)
-      .then(function (books) {
+      .then(function(books) {
         var $container = $('<div />');
-        $.get('app/partials/book-id-partial.html', function (templateString) {
+        $.get('app/partials/book-id-partial.html', function(templateString) {
           var template = handlebars.compile(templateString);
           books.map(template)
-            .forEach(function (bookhtml) {
+            .forEach(function(bookhtml) {
               $container.append(bookhtml);
             });
           $('#content').html($container.html());
+
+          $('.btn-change-status').on('click', function() {
+            var $this = $(this);
+            var status = $this.attr('data-status');
+            var bookId = $this.parents('.book-container').attr('data-id');
+            console.log(bookId);
+            data.myBooks.changeStatus(bookId, status);
+          });
         });
       });
 
 
   });
 
-  this.get('#/login', function (context) {
-    $.get('app/partials/login-partial.html', function (html) {
+  this.get('#/login', function(context) {
+    $.get('app/partials/login-partial.html', function(html) {
       $('#content').html(html);
 
-      $('#sign-in').on('click', function () {
+      $('#sign-in').on('click', function() {
         var username = $('#username').val();
         var password = $('#password').val();
 
@@ -41,28 +49,28 @@ var sammyApp = Sammy('#content', function () {
             username: username,
             password: password
           })
-          .then(function (user) {
+          .then(function(user) {
 
             context.redirect('#/books');
 
 
-          }, function (err) {
+          }, function(err) {
             alert(JSON.stringify(err));
           });
       });
 
-      $('#register').on('click', function () {
+      $('#register').on('click', function() {
         context.redirect('#/register');
       });
 
     });
   });
 
-  this.get('#/register', function (context) {
-    $.get('app/partials/register-partial.html', function (html) {
+  this.get('#/register', function(context) {
+    $.get('app/partials/register-partial.html', function(html) {
       $('#content').html(html);
 
-      $('#register').on('click', function () {
+      $('#register').on('click', function() {
         var username = $('#username').val();
         var password = $('#password').val();
 
@@ -71,9 +79,9 @@ var sammyApp = Sammy('#content', function () {
             username: username,
             password: password
           })
-          .then(function (user) {
+          .then(function(user) {
             context.redirect('#/login');
-          }, function (err) {
+          }, function(err) {
             alert(JSON.stringify(err));
           });
 
@@ -81,17 +89,17 @@ var sammyApp = Sammy('#content', function () {
     });
   });
 
-  this.get('#/books/add', function (context) {
+  this.get('#/books/add', function(context) {
     //context === this
 
-    $.get('app/partials/book-add-partial.html', function (html) {
+    $.get('app/partials/book-add-partial.html', function(html) {
       // body...
       $('#content').html(html);
 
       var $button = $('#add-book')
-        .on('click', function () {
+        .on('click', function() {
           var genres = [];
-          $('.tb-genre').each(function (index, genre) {
+          $('.tb-genre').each(function(index, genre) {
             var value = $(genre).val();
             if (value) {
               genres.push(value);
@@ -105,27 +113,27 @@ var sammyApp = Sammy('#content', function () {
               pages: $('#tb-pages').val()
 
             })
-            .then(function (book) {
+            .then(function(book) {
               context.redirect('#/books/' + book._id);
             });
         });
     });
   });
 
-  this.get('#/books/:id/edit', function (context) {
+  this.get('#/books/:id/edit', function(context) {
     var bookId = this.params.id;
     data.books.getById(bookId)
-      .then(function (book) {
-        $.get('app/partials/book-edit-partial.html', function (templateString) {
+      .then(function(book) {
+        $.get('app/partials/book-edit-partial.html', function(templateString) {
           var template = handlebars.compile(templateString);
           var html = template(book);
           $('#content').html(html);
 
           $('#save')
-            .on('click', function () {
+            .on('click', function() {
 
               var genres = [];
-              $('.tb-genre').each(function (index, genre) {
+              $('.tb-genre').each(function(index, genre) {
                 var value = $(genre).val();
                 if (value) {
                   genres.push(value);
@@ -140,7 +148,7 @@ var sammyApp = Sammy('#content', function () {
                   description: $('#tb-description').val(),
                   pages: $('#tb-pages').val()
                 })
-                .then(function (book) {
+                .then(function(book) {
                   context.redirect('#/books/' + book._id);
                 });
             });
@@ -149,25 +157,33 @@ var sammyApp = Sammy('#content', function () {
       });
   });
 
-  this.get('#/books/:id', function (context) {
+  this.get('#/books/:id', function(context) {
     var that = this;
     var bookId = this.params.id;
     data.books.getById(bookId)
-      .then(function (book) {
-        $.get('app/partials/book-id-partial.html', function (templateString) {
+      .then(function(book) {
+        $.get('app/partials/book-id-partial.html', function(templateString) {
           var template = handlebars.compile(templateString);
           var html = template(book);
           $('#content').html(html);
+
+          $('.btn-change-status').on('click', function() {
+            var $this = $(this);
+            var status = $this.attr('data-status');
+            var bookId = $this.parents('.book-container').attr('data-id');
+            console.log(bookId);
+            data.myBooks.changeStatus(bookId, status);
+          });
         });
       });
   });
 
-  this.get('#/genres', function () {
+  this.get('#/genres', function() {
     var genre = this.params.genre;
     data.genres.get()
-      .then(function (genres) {
+      .then(function(genres) {
         var $container = $('<div />');
-        $.get('app/partials/genres-all-partial.html', function (templateString) {
+        $.get('app/partials/genres-all-partial.html', function(templateString) {
           var template = handlebars.compile(templateString);
 
           var html = template({
@@ -179,7 +195,7 @@ var sammyApp = Sammy('#content', function () {
   });
 });
 
-$(function () {
+$(function() {
   sammyApp.run('#/');
 });
 
