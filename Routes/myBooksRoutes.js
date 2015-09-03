8d232916c0e06bb55a200e79992f1fefb4932dc4
GeriCookie <gergana.bailova@gmail.com>
2015-09-03 23:16:50 +0100

    Added myBooksRoutes

--- Routes/myBooksRoutes.js
+++ Routes/myBooksRoutes.js
@@ -1,13 +1,132 @@
 var express = require('express');
 
-var routes = function(User, Book, Update) {
-  var myBooksRouter = express.Router();
+var routes = function (User, Book, Update) {
+	var myBooksRouter = express.Router();
 
-  var myBooksController = require('../Controllers/myBooksController')(User, Book, Update);
-  myBooksRouter.route('/')
-    .get(myBooksController.get)
-    .put(myBooksController.put);
-  return myBooksRouter;
+	var myBooksController = require('../Controllers/myBooksController')(User, Book, Update);
+	myBooksRouter.route('/')
+		.get(myBooksController.get)
+		.put(myBooksController.put);
+
+
+	myBooksRouter.route('/all')
+		.get(function (req, res) {
+			var user = req.user;
+			if (!user) {
+				res.status(401);
+				res.json({
+					message: 'Unauthorized user'
+				});
+				return;
+			}
+			var books = user.booksToRead.concat(user.booksCurrentlyReading).concat(user.booksRead);
+
+			var bookIds = books.map(function (book) {
+				return book.id;
+			});
+			Book.find({
+				_id: {
+					"$in": bookIds
+				}
+			}, function (err, books) {
+				if (err) {
+					throw err;
+				}
+				res.json(books);
+			});
+		});
+
+	myBooksRouter.route('/to-read')
+		.get(function (req, res) {
+			var user = req.user;
+			if (!user) {
+				res.status(401);
+				res.json({
+					message: 'Unathorized user'
+				});
+				return;
+			}
+
+			var books = user.booksToRead;
+
+			var bookIds = books.map(function (book) {
+				return book.id;
+			});
+
+			Book.find({
+				_id: {
+					"$in": bookIds
+				}
+			}, function (err, books) {
+				if (err) {
+					throw err;
+				}
+				res.json(books);
+			});
+		});
+
+	myBooksRouter.route('/currently-reading')
+		.get(function (req, res) {
+			var user = req.user;
+			if (!user) {
+				res.status(401);
+				res.json({
+					message: 'Unathorized user'
+				});
+				return;
+			}
+
+			var books = user.booksCurrentlyReading;
+
+			var bookIds = books.map(function (book) {
+				return book.id;
+			});
+
+			Book.find({
+				_id: {
+					"$in": bookIds
+				}
+			}, function (err, books) {
+				if (err) {
+					throw err;
+				}
+				res.json(books);
+			});
+		});
+
+
+	myBooksRouter.route('/read')
+		.get(function (req, res) {
+			var user = req.user;
+			if (!user) {
+				res.status(401);
+				res.json({
+					message: 'Unathorized user'
+				});
+				return;
+			}
+
+			var books = user.booksRead;
+
+			var bookIds = books.map(function (book) {
+				return book.id;
+			});
+
+			Book.find({
+				_id: {
+					"$in": bookIds
+				}
+			}, function (err, books) {
+				if (err) {
+					throw err;
+				}
+				res.json(books);
+			});
+		});
+
+
+
+	return myBooksRouter;
 };
 
 module.exports = routes;
