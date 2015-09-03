00a68c51efbf9052cc57cb58a16a3358e263f94a
Lyubo <Lyubomir.Yonchev@gmail.com>
2015-09-03 22:22:15 +0300

    added basic unit tests for books
    added basic unit tests for books

--- public/tests.js
+++ public/tests.js
@@ -1,6 +1,27 @@
-import data from 'app/data/data.js'
+import data from 'app/data/data';
 
 mocha.setup('bdd');
 var expect = chai.expect;
 
-define('')
+
+describe('#sum', function() {
+    it('expect to have at least one book in database', function(done){
+        data.books.get()
+            .then(function (books) {
+                expect(books).to.not.be.empty;
+                done();
+            });
+    });
+    it('expect first book to all correct properties for object Book', function(done){
+        data.books.get()
+            .then(function (books) {
+                console.log(books[0]);
+                expect(books[0]).to.have.property('title');
+                //expect(books[0]).to.have.property('author');
+                expect(books[0]).to.have.property('reviews');
+                expect(books[0]).to.have.property('genres');
+
+                done();
+            });
+    });
+})
