477b90b830d3fbb090ea83312bf513379b313a6a
juvemar <martoo.atanasov@gmail.com>
2015-09-04 14:49:55 +0300

    Title changed

--- public/systemjs-tests-config.js
+++ public/systemjs-tests-config.js
@@ -1,11 +1,11 @@
 System.config({
     transpiler: 'babel',
     map: {
-        babel: 'node_modules/babel-core/browser.js'
+        babel: '../node_modules/babel-core/browser.js'
     }
 });
 
-System.import('tests.js')
+System.import('../tests/tests.js')
     .then(function () {
         mocha.run();
     }, function (err) {
