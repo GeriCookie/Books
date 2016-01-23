0f409db39fd8ade9c40689c07538f39fcb63b0ed
GeriCookie <gergana.bailova@gmail.com>
2016-01-22 16:27:53 -0800

    Fixed a bug

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express'),
-  bodyParser = require('body-parser'),
-  mongoose = require('mongoose'),
-  cors = require('cors');
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    cors = require('cors');
 
 // db = mongoose.connect('mongodb://localhost/books');
 
@@ -24,35 +24,35 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-app.use('/', function (req, res, next) {
-  var authKey = req.headers['x-auth-key'];
-  //console.log(authKey);
-  if (!authKey) {
-    next();
-    return;
-  }
-  User.find({
-      authKey: authKey
-    })
-    .then(function (users) {
-      var user = users[0];
-      req.user = user || null;
-      next();
-    });
+app.use('/', function(req, res, next) {
+    var authKey = req.headers['x-auth-key'];
+    //console.log(authKey);
+    if (!authKey) {
+        next();
+        return;
+    }
+    User.find({
+            authKey: authKey
+        })
+        .then(function(users) {
+            var user = users[0];
+            req.user = user || null;
+            next();
+        });
 });
 
-app.get('/api/book-pages', function (req, res) {
-  var size = req.query.size || 10;
-  Book.find({}, function (err, books) {
-    if (err) {
-      res.status(404).send(err);
-    }
-    var booksCount = books.length;
-    var pagesCount = Math.floor(booksCount / size) + 1;
-    res.json({
-      pages: pagesCount
+app.get('/api/book-pages', function(req, res) {
+    var size = req.query.size || 10;
+    Book.find({}, function(err, books) {
+        if (err) {
+            res.status(404).send(err);
+        }
+        var booksCount = books.length;
+        var pagesCount = Math.floor(booksCount / size) + 1;
+        res.json({
+            pages: pagesCount
+        });
     });
-  });
 });
 
 var bookRouter = require('./Routes/bookRoutes')(Book, Update);
@@ -60,6 +60,7 @@ var userRouter = require('./Routes/userRoutes')(User);
 var myBooksRouter = require('./Routes/myBooksRoutes')(User, Book, Update);
 var searchRouter = require('./Routes/searchRoutes')(Book);
 var updateRouter = require('./Routes/updateRoutes')(User, Book, Update);
+
 app.use('/api/books', bookRouter);
 app.use('/api/authors', bookRouter);
 app.use('/api/users', userRouter);
@@ -67,22 +68,26 @@ app.use('/api/mybooks', myBooksRouter);
 app.use('/api/search', searchRouter);
 app.use('/api/updates', updateRouter);
 
-app.get('/api/genres', function (req, res) {
-  Book.find({}, function (err, books) {
-    var genres = {};
-    books.forEach(function (book) {
-      book.genres.forEach(function (genre) {
-        genres[genre] = true;
-      });
+app.get('/api/genres', function(req, res) {
+    Book.find({}, function(err, books) {
+        var genres = {};
+        books.forEach(function(book) {
+            if (!book.genres) {
+                return;
+            }
 
+            book.genres.forEach(function(genre) {
+                genres[genre] = true;
+            });
+
+        });
+        genres = Object.keys(genres);
+        res.json(genres);
     });
-    genres = Object.keys(genres);
-    res.json(genres);
-  });
 });
 
-app.listen(port, function () {
-  console.log('Running on PORT: ' + port);
+app.listen(port, function() {
+    console.log('Running on PORT: ' + port);
 });
 
 module.exports = app;
